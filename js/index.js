82ca478d2076280fd14936955877589d58b20958
Hailagaz <omentielvo.lumenn@gmail.com>
2022-05-19 13:28:51 +0300

    new

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// MENU
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+function hrefOnClick() {
+    document.getElementById("menu-bar").classList.remove("change");
+    document.getElementById("nav").classList.remove("change");
+    document.getElementById("menu-bg").classList.remove("change-bg");
+}
