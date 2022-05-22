fd05a93db94488b82ed093a2a7cdd11f1e64dec3
Hailagaz <omentielvo.lumenn@gmail.com>
2022-05-22 14:20:44 +0300

    Add fake API request for gettig testimonials

--- js/index.js
+++ js/index.js
@@ -1,24 +1,3 @@
-// SWIPER
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //direction: 'vertical',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
 
 // MENU
 
@@ -33,3 +12,64 @@ function hrefOnClick() {
     document.getElementById("nav").classList.remove("change");
     document.getElementById("menu-bg").classList.remove("change-bg");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        fetch("../data/testimonials.json")
+            .then((response) => response.json())
+            .then((data) => {
+                let slides = '';
+
+                for (let testimonial of data) {
+                    slides += `
+                        <div class="swiper-slide">
+                            <div class="swiper-info">
+                                <img class="swiper-photo" src="images/image_1.png" alt="Testimonials, person 1">
+                                <div class="swiper-person">
+                                    <div class="swiper-name">
+                                        ${testimonial.name}
+                                    </div>
+                                    <div class="swiper-position">
+                                        ${testimonial.position}
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="swiper-testimonials">
+                                ${testimonial.testimonial}
+                            </p>
+                        </div>
+                    `;
+                }
+                document.querySelector('.swiper').innerHTML = `
+                    <div class="swiper-wrapper">
+                        ${slides}
+                    </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                `;
+                
+                // SWIPER
+                const swiper = new Swiper('.swiper', {
+                    // Optional parameters
+                    //direction: 'vertical',
+                    loop: true,
+                    slidesPerView: 1,
+                    spaceBetween: 30,
+                    // If we need pagination
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+                    // Navigation arrows
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                });
+            })
+            .catch((err) => {
+                document.querySelector('.swiper').innerHTML = '<p>Sorry, oops!</p>';
+            });
+    }, 2000);
+})
