82b73f5d3678ed26d4b68b92d4a520a3d6961b63
Hailagaz <omentielvo.lumenn@gmail.com>
2022-05-26 10:47:10 +0300

    Fix scroll position after refresh

--- js/index.js
+++ js/index.js
@@ -14,6 +14,11 @@ function hrefOnClick() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    if(window.location.hash) {
+        document.querySelector(window.location.hash).scrollIntoView();
+    }
+
     fetch("../data/testimonials.json")
         .then((response) => response.json())
         .then((data) => {
@@ -70,55 +75,3 @@ document.addEventListener("DOMContentLoaded", () => {
             document.querySelector('.swiper').innerHTML = '<p>Sorry, oops!</p>';
         });
 })
-//Scroll var1
-// let cords = ['scrollX', 'scrollY'];
-// // Перед закрытием записываем в локалсторадж window.scrollX и window.scrollY как scrollX и scrollY
-// window.addEventListener('unload', e => cords.forEach(cord => localStorage[cord] = window[cord]));
-// // Прокручиваем страницу к scrollX и scrollY из localStorage (либо 0,0 если там еще ничего нет)
-// window.scroll(...cords.map(cord => localStorage[cord]));
-
-
-//Scroll var2
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     var scrollpos = localStorage.getItem('scrollpos');
-//     if (scrollpos) window.scrollTo(0, scrollpos);
-// });
-
-// window.onbeforeunload = function (e) {
-//     localStorage.setItem('scrollpos', window.scrollY);
-// };
-
-
-//Scroll var3
-// //code to refresh the page
-// var page_y = $(document).scrollTop();
-// window.location.href = window.location.href + '?page_y=' + page_y;
-
-// //code to handle setting page offset on load
-// $(function () {
-//     if (window.location.href.indexOf('page_y') != -1) {
-//         //gets the number from end of url
-//         var match = window.location.href.split('?')[1].match(/\d+$/);
-//         var page_y = match[0];
-
-//         //sets the page offset
-//         $('html, body').scrollTop(page_y);
-//     }
-// });
-
-
-//Scroll var4
-document.addEventListener("DOMContentLoaded", function (event) {
-    var scrollpos = sessionStorage.getItem('scrollpos');
-    if (scrollpos) {
-        window.scrollTo(0, scrollpos);
-        sessionStorage.removeItem('scrollpos');
-    }
-});
-
-window.addEventListener("beforeunload", function (e) {
-    sessionStorage.setItem('scrollpos', window.scrollY);
-});
-
-
-
