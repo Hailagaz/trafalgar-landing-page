125d2c1ae2e9ba7c2f6c18f9bb250ec1e3d59fad
Hailagaz <omentielvo.lumenn@gmail.com>
2022-06-17 14:51:13 +0300

    Fix download button for html form

--- js/form.js
+++ js/form.js
@@ -1,15 +1,15 @@
-function ready() {
-    var inputs = document.querySelectorAll('.form__load');
-    Array.prototype.forEach.call(inputs, function(input) {
+document.addEventListener("DOMContentLoaded", function () {
+    var inputs = document.querySelectorAll('.form__file-input');
+    Array.prototype.forEach.call(inputs, function (input) {
         var label = input.nextElementSibling;
         var labelVal = label.innerHTML;
 
-        input.addEventListener('change', function(e) {
+        input.addEventListener('change', function (e) {
             console.log(this.files);
-            var fileName='';
+            var fileName = '';
             if (this.files && this.files.length > 1) {
                 fileName = (this.getAttribute('data-multiple-caption') || '')
-                .replace('{count}', this.files.length);
+                    .replace('{count}', this.files.length);
             } else {
                 fileName = this.files[0].name;
             }
@@ -21,6 +21,4 @@ function ready() {
             }
         });
     });
-};
-
-document.addEventListener("DOMContentLoaded", ready);
+});
